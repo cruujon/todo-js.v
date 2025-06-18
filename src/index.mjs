d7c04b6a3497aacc8c863c1229a4c68f31c3f5ba
cruujon.eth <huitaiheiyan3@gmail.com>
2025-06-18 06:23:37 +0000

    2025/06/18

--- src/index.mjs
+++ src/index.mjs
@@ -22,14 +22,25 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    // 押された完了ボタンの親にあるliタグ配下の完了ボタンと削除ボタンを削除
+    const moveTarget = completeButton.closest("li");
+    completeButton.nextElementSibling.remove();
+    completeButton.remove();
+    //戻すボタンを生成してdivタグ配下に設定
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    moveTarget.firstElementChild.appendChild(backButton);
+    //　完了リストに移動
+    document.getElementById("complete-list").appendChild(moveTarget);
   });
 
   //button(削除)タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("削除");
+    //押された削除ボタンの親にあるliタグを未完了リストからさくら削除
+    const deleteTarget = deleteButton.closest("li");
+    document.getElementById("incomplete-list").removeChild(deleteTarget)
   });
 
   // liタグの子要素に各要素を設定
